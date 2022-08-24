b9c6a23b6bbce241723559639d175ebd367ef25f
Kurosirogitune <69235373+Kurosirogitune@users.noreply.github.com>
2022-08-24 20:27:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ if button:
 expander = st.expander('問い合わせ')
 expander.write('問い合わせ内容')
 
-#text = st.text_input('あなたの趣味を教えてください')
-#condition = st.slider('あなたの調子は',0,100,50)
+text = st.text_input('あなたの趣味を教えてください')
+condition = st.slider('あなたの調子は',0,100,50)
 
-#'私のシュミは',text
-#'私の調子は', condition
+'私のシュミは',text
+'私の調子は', condition
 
 #セレクトボックスの中から好きな文字を選んで代入
 #text = st.text_input('あなたの趣味を教えてください')
@@ -46,9 +46,9 @@ expander.write('問い合わせ内容')
 #'あなたの好きな数字は',option,'です'
 
 #チェックボックスにチェックを入れると画像表示
-#if st.checkbox('Show Image'):
-#    img = Image.open('starwars.png')
-#    st.image(img,caption='スターウォーズ',use_column_width=True)
+if st.checkbox('Show Image'):
+    img = Image.open('starwars.png')
+    st.image(img,caption='スターウォーズ',use_column_width=True)
 
 #マッピング姫路城#割り算した小さい値に姫路城の緯度経度を基にマッピングする
 # df = pd.DataFrame(
